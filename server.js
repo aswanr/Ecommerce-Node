dfb2412a44abeded982c4e3bc895118c94cb9211
Aswan R <aswan.r@coddletech.com>
2025-01-02 11:15:49 +0530

    first commit

--- server.js
+++ server.js
@@ -1,35 +1,38 @@
 const express = require("express");
-const expr = express();
-const mysql=require("mysql2");
-const path=require("path");
+const app = express();
+const mysql = require("mysql2");
+const path = require("path");
 
-const db=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"ecommerse_db",
+
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "ecommerse_db",
 })
 
-expr.get('/',(req,res)=>{
-        db.query("SELECT * from cart",(err,result) =>{
-            if(err){
-                console.log(`unfounded ${err}`);
-                return;
-            }
-            res.json(result);
-        });
-
+app.get('/', (req, res) => {
+    db.query("select * from cart", (err, result) => {
+        if (err) {
+            console.log(`unfounded ${err}`);
+            return;
+        }
+        res.json(result);
     });
-expr.get('/first',(req,res)=>{
-    res.sendFile(path.join(__dirname,"./routerpages","first.html"));
+
+});
+
+app.get('/first', (req, res) => {
+    res.sendFile(path.join(__dirname, "./routerpages", "first.html"));
 })
-expr.get('/second',(req,res)=>{
-    res.sendFile(path.join(__dirname,"./routerpages","second.html"));
+
+app.get('/second', (req, res) => {
+    res.sendFile(path.join(__dirname, "./routerpages", "second.html"));
 })
 
 const port=process.env.port || 3001;
 
-expr.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server running on Port ${port}`);
 })
-
